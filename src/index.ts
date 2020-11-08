f7f951fa9c22c9868e0f7ff0139825fc8cdaff5e
Jiri Srba <jiri.srba@gmail.com>
2020-11-08 18:25:43 +0100

    add json logger

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,58 @@
-import { app } from "./app";
+import express, { Application } from "express";
+
+import { Movie } from "./db/models/Movie.model";
+import { connect } from "./db/db";
+
+// Import core components
+import { log } from "./core/helpers/Logger";
+
 import { AddressInfo } from "net";
 import "./config";
+
+connect();
+
+const app: Application = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.get(['/', '/health', '/status'], (req, res) => {
+  console.log('status: "UP"');
+  return res.status(200).json({ status: "UP" });
+  })
+
+app.post("/movies", async (req, res) => {
+  const movie = new Movie();
+  movie.title = req.body.title;
+  movie.plot_summary = req.body.plot_summary;
+  movie.duration = req.body.duration;
+  await movie.save();
+  res.send(movie);
+});
+
+app.get("/movies", async (req, res) => {
+  console.info("get movies")
+  const movies = await Movie.find();
+  res.send(movies);
+});
+
+app.get("/movies/:id", async (req, res) => {
+  const movie = await Movie.findOne({
+    where: {
+      id: req.params.id,
+    },
+  });
+  if (movie) {
+    res.json(movie);
+  } else {
+    res.status(404).send({ message: "Movie not found" });
+  }
+});
 
 const PORT: number = parseInt(process.env.PORT) || 3000;
 
 const server = app.listen(Number(PORT), "0.0.0.0", () => {
   const { port, address } = server.address() as AddressInfo;
-  console.log("Server listening on:", "http://" + address + ":" + port);
+  log.info("Server listening on:", "http://" + address + ":" + port);
 });
